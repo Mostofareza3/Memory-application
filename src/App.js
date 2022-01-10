ec0e9686702c592e88073a3804067d4e93836775
Mostofareza3 <86896410+Mostofareza3@users.noreply.github.com>
2022-01-10 16:16:16 +0600

    [Redux setup]

--- src/App.js
+++ src/App.js
@@ -1,45 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import {AppBar, Container, Grid, Grow, Typography} from '@material-ui/core'
-import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
-import useStyle from './styles';
-import { useDispatch } from 'react-redux';
-import {getPosts} from './action/posts'
+import React from 'react';
+import { Container } from '@material-ui/core'
+import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
 
 
 
 const App = () => {
-    const [currentId, setCurrentId] = useState(null);
-    const classes = useStyle();
-    const dispatch = useDispatch();
 
-    useEffect(()=>{
-        dispatch(getPosts());
-
-    },[currentId,dispatch])
     return (
-        <Container maxWidth="lg">
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">
-                    Memories
-                    <img className={classes.image} src="https://raw.githubusercontent.com/adrianhajdin/project_mern_memories/master/client/src/images/memories.png?token=AF56X74XONEUGZ4FD2FUIA27UURPI" alt="memories" height="60"/>
-                </Typography>
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid className={classes.mailContainer} container justifyContent="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId={setCurrentId}/>
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId}/>
-                        </Grid>
+        <BrowserRouter>
 
-                    </Grid>
-                </Container>
-            </Grow>
+            <Container maxWidth="lg">
+                <Navbar />
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/auth">
+                        <Auth />
+                    </Route>
 
-        </Container>
+                </Switch>
+            </Container>
+        </BrowserRouter>
     );
 };
 
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,47 @@
+import { Typography, AppBar, Toolbar, Avatar, Button } from '@material-ui/core'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import useStyle from './styles'
+
+
+export default function Navbar() {
+
+    const classes = useStyle();
+
+    const user = null;
+
+    return (
+        <>
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <div className={classes.brandContainer}>
+
+                    <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">
+                        Memories
+                    </Typography>
+                    <img className={classes.image} src="https://raw.githubusercontent.com/adrianhajdin/project_mern_memories/master/client/src/images/memories.png?token=AF56X74XONEUGZ4FD2FUIA27UURPI" alt="memories" height="60" />
+                    </div>
+                    <Toolbar className={classes.toolbar}>
+                        {user ? (
+                            <div className={classes.profile}>
+                                <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>
+                                    {user.result.name.charAt(0)}
+                                </Avatar>
+                                <Typography className={classes.userName} variant="h6">
+                                    {user.result.name}
+                                </Typography>
+                                <Button className={classes.logout} variant="contained" color="secondary">Logout</Button>
+                            </div>
+
+                        ) : (
+                           
+
+                            <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+                            
+                        )
+                        }
+                    </Toolbar>
+                
+            </AppBar>
+        </>
+    )
+}
