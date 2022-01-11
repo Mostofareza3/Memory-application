d9a77995bf2d777a3bc22ccee0ebe3875942e6e8
Mostofareza3 <86896410+Mostofareza3@users.noreply.github.com>
2022-01-11 14:25:32 +0600

    [Like, Delete, Edit functionality added]

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -6,28 +6,50 @@ import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import DeleteIcon from '@material-ui/icons/Delete';
 import moment from 'moment';
 import { useDispatch } from 'react-redux';
-import {deletePost, likePost} from '../../../action/posts';
+import { deletePost, likePost } from '../../../action/posts';
+import ThumbUpAltOutlined from '@material-ui/icons/ThumbUpAltOutlined';
 
 const Post = ({ post, setCurrentId }) => {
-    
+
     const classes = useStyle();
     const dispatch = useDispatch();
+    const user = JSON.parse(localStorage.getItem('profile'));
+
+    // like count controller sub-component.
+    const Likes = () => {
+        if (post.likes.length > 0) {
+            return post.likes.find((like) => like === (user?.result?.googleId || user?.result?._id))
+                ? (
+                    <><ThumbUpAltIcon fontSize="small" />&nbsp;{post.likes.length > 2 ? `You and ${post.likes.length - 1} others` : `${post.likes.length} like${post.likes.length > 1 ? 's' : ''}`}</>
+                ) : (
+                    <><ThumbUpAltOutlined fontSize="small" />&nbsp;{post.likes.length} {post.likes.length === 1 ? 'Like' : 'Likes'}</>
+                );
+        }
+
+        return <><ThumbUpAltOutlined fontSize="small" />&nbsp;Like</>;
+    };
 
     return (
         <Card className={classes.card}>
             <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
+
             <div className={classes.overlay}>
                 <Typography variant="h6">{post?.name}</Typography>
                 <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
             </div>
-            <div className={classes.overlay2}>
-                <Button
-                    style={{ color: 'white' }}
-                    size="small"
-                    onClick={() => { setCurrentId(post._id) }}>
-                    <MoreHorizIcon fontSize="medium" />
-                </Button>
-            </div>
+
+            {/* ----check is post creator it is or not? by using google login or manual login to show Edit button */}
+
+            {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
+                <div className={classes.overlay2}>
+                    <Button
+                        style={{ color: 'white' }}
+                        size="small"
+                        onClick={() => { setCurrentId(post._id) }}>
+                        <MoreHorizIcon fontSize="medium" />
+                    </Button>
+                </div>
+            )}
             <div className={classes.details}>
                 <Typography variant="body2" color="textSecondary">{post.tags.map((tag) => `#${tag} `)}</Typography>
 
@@ -37,15 +59,23 @@ const Post = ({ post, setCurrentId }) => {
                 <Typography color="textSecondary" variant="body2" component="p">{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={() => { dispatch(likePost(post._id)) }}>
-                    <ThumbUpAltIcon fontSize="small" />
-                    &nbsp; Like &nbsp;
-                    {post.likeCount}
+                {/* ------like button  ------   */}
+                <Button size="small"
+                    color="primary"
+                    dispatch={!user?.result}
+                    onClick={() => { dispatch(likePost(post._id)) }}>
+                    {/* sub component */}
+                    <Likes />
+
                 </Button>
-                <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
-                    <DeleteIcon fontSize="small" />
-                    Delete
-                </Button>
+                {/* ----check is post creator it is or not? by using google login or manual login to show delete button */}
+                {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
+
+                    <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
+                        <DeleteIcon fontSize="small" />
+                        Delete
+                    </Button>
+                )}
 
             </CardActions>
         </Card>
